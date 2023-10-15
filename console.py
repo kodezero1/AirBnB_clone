ba36b94a0653ac54cfc107dd61f915e91096b0ab
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-10-15 22:36:10 +0100

    Added console.py

---
+++ console.py
@@ -0,0 +1,19 @@
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF (Ctrl+D)"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
